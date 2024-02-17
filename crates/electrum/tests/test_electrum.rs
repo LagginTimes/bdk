7162c4a1e736a6ab9fd51305077110b85e56b74a
vmammal <valuedmammal@protonmail.com>
2024-02-17 12:45:16 -0500

    test(electrum): Add test `update_tx_graph_gap_limit`

--- crates/electrum/tests/test_electrum.rs
+++ crates/electrum/tests/test_electrum.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use bdk_chain::{
-    bitcoin::{hashes::Hash, Address, Amount, ScriptBuf, WScriptHash},
+    bitcoin::{hashes::Hash, Address, Amount, ScriptBuf, Txid, WScriptHash},
     keychain::Balance,
-    local_chain::LocalChain,
-    ConfirmationTimeHeightAnchor, IndexedTxGraph, SpkTxOutIndex,
+    local_chain::{CheckPoint, LocalChain},
+    BlockId, ConfirmationTimeHeightAnchor, IndexedTxGraph, SpkTxOutIndex,
 };
 use bdk_electrum::{ElectrumExt, ElectrumUpdate};
 use bdk_testenv::TestEnv;
@@ -220,6 +220,120 @@ fn tx_can_become_unconfirmed_after_reorg() -> Result<()> {
             depth,
         );
     }
+
+    Ok(())
+}
+
+#[test]
+fn update_tx_graph_gap_limit() -> Result<()> {
+    use std::collections::{BTreeMap, HashSet};
+    use std::str::FromStr;
+
+    let env = TestEnv::new()?;
+    let client = electrum_client::Client::new(env.electrsd.electrum_url.as_str())?;
+
+    // Now let's test the gap limit. First get 10 new addresses and index them.
+    let addresses: Vec<Address> = [
+        "bcrt1qj9f7r8r3p2y0sqf4r3r62qysmkuh0fzep473d2ar7rcz64wqvhssjgf0z4",
+        "bcrt1qmm5t0ch7vh2hryx9ctq3mswexcugqe4atkpkl2tetm8merqkthas3w7q30",
+        "bcrt1qut9p7ej7l7lhyvekj28xknn8gnugtym4d5qvnp5shrsr4nksmfqsmyn87g",
+        "bcrt1qqz0xtn3m235p2k96f5wa2dqukg6shxn9n3txe8arlrhjh5p744hsd957ww",
+        "bcrt1q9c0t62a8l6wfytmf2t9lfj35avadk3mm8g4p3l84tp6rl66m48sqrme7wu",
+        "bcrt1qkmh8yrk2v47cklt8dytk8f3ammcwa4q7dzattedzfhqzvfwwgyzsg59zrh",
+        "bcrt1qvgrsrzy07gjkkfr5luplt0azxtfwmwq5t62gum5jr7zwcvep2acs8hhnp2",
+        "bcrt1qw57edarcg50ansq8mk3guyrk78rk0fwvrds5xvqeupteu848zayq549av8",
+        "bcrt1qvtve5ekf6e5kzs68knvnt2phfw6a0yjqrlgat392m6zt9jsvyxhqfx67ef",
+        "bcrt1qw03ddumfs9z0kcu76ln7jrjfdwam20qtffmkcral3qtza90sp9kqm787uk",
+    ]
+    .into_iter()
+    .map(|s| Address::from_str(s).unwrap().assume_checked())
+    .collect();
+    let spks: Vec<(u32, ScriptBuf)> = addresses
+        .iter()
+        .enumerate()
+        .map(|(i, addr)| (i as u32, addr.script_pubkey()))
+        .collect();
+
+    let mut keychain_spks = BTreeMap::new();
+    keychain_spks.insert(0, spks);
+    let tx_graph = IndexedTxGraph::<ConfirmationTimeHeightAnchor, _>::new({
+        let mut index = SpkTxOutIndex::default();
+        for (i, spk) in keychain_spks.get(&0).unwrap() {
+            index.insert_spk(i, spk.clone());
+        }
+        index
+    });
+
+    // Mine blocks.
+    let block_hashes = env.mine_blocks(101, None)?;
+    let prev_tip = CheckPoint::new(BlockId {
+        height: 1,
+        hash: block_hashes[0],
+    });
+
+    // Then receive coins on the 4th address.
+    let txid_4th_addr = env.bitcoind.client.send_to_address(
+        &addresses[3],
+        Amount::from_sat(10000),
+        None,
+        None,
+        None,
+        None,
+        Some(1),
+        None,
+    )?;
+    let _ = env.mine_blocks(1, None)?;
+    env.wait_until_electrum_sees_block()?;
+
+    // A scan with a gap limit of 2 won't find the transaction, but a scan with a gap limit of 3 will.
+    // FIXME: See <http://github.com/bitcoindevkit/bdk/pull/1351> which changes the behavior of `stop_gap`
+    let (ElectrumUpdate { relevant_txids, .. }, active_indices) =
+        client.full_scan(prev_tip.clone(), keychain_spks.clone(), 2, 1)?;
+    let missing = relevant_txids.missing_full_txs(tx_graph.graph());
+    let graph_update = relevant_txids.into_confirmation_time_tx_graph(&client, None, missing)?;
+    assert!(graph_update.full_txs().next().is_none());
+    assert!(active_indices.is_empty());
+
+    let (ElectrumUpdate { relevant_txids, .. }, active_indices) =
+        client.full_scan(prev_tip.clone(), keychain_spks.clone(), 3, 1)?;
+    let missing = relevant_txids.missing_full_txs(tx_graph.graph());
+    let graph_update = relevant_txids.into_confirmation_time_tx_graph(&client, None, missing)?;
+    assert_eq!(graph_update.full_txs().next().unwrap().txid, txid_4th_addr);
+    assert_eq!(active_indices[&0], 3);
+
+    // Now receive a coin on the last address.
+    let txid_last_addr = env.bitcoind.client.send_to_address(
+        &addresses[addresses.len() - 1],
+        Amount::from_sat(10000),
+        None,
+        None,
+        None,
+        None,
+        Some(1),
+        None,
+    )?;
+    let _ = env.mine_blocks(1, None)?;
+    env.wait_until_electrum_sees_block()?;
+
+    // A scan with gap limit 4 won't find the second transaction, but a scan with gap limit 5 will.
+    // The last active index won't be updated in the first case but will in the second.
+    let (ElectrumUpdate { relevant_txids, .. }, active_indices) =
+        client.full_scan(prev_tip.clone(), keychain_spks.clone(), 4, 1)?;
+    let missing = relevant_txids.missing_full_txs(tx_graph.graph());
+    let graph_update = relevant_txids.into_confirmation_time_tx_graph(&client, None, missing)?;
+    let txids: HashSet<Txid> = graph_update.full_txs().map(|tx| tx.txid).collect();
+    assert_eq!(txids.len(), 1);
+    assert!(txids.contains(&txid_4th_addr));
+    assert_eq!(active_indices[&0], 3);
+
+    let (ElectrumUpdate { relevant_txids, .. }, active_indices) =
+        client.full_scan(prev_tip, keychain_spks.clone(), 5, 1)?;
+    let missing = relevant_txids.missing_full_txs(tx_graph.graph());
+    let graph_update = relevant_txids.into_confirmation_time_tx_graph(&client, None, missing)?;
+    let txids: HashSet<Txid> = graph_update.full_txs().map(|tx| tx.txid).collect();
+    assert_eq!(txids.len(), 2);
+    assert!(txids.contains(&txid_4th_addr) && txids.contains(&txid_last_addr));
+    assert_eq!(active_indices[&0], 9);
 
     Ok(())
 }
